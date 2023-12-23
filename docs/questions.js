530facf49d3825175ab4d5a65bf040bef1049d34
dhruvaggarwal005 <154580852+dhruvaggarwal005@users.noreply.github.com>
2023-12-23 10:51:06 +0530

    Create questions.js

---
+++ docs/questions.js
@@ -0,0 +1,109 @@
+let questions=[{
+    num:1,
+    question:"What does HTML stand for?",
+    answer:"B. Hypertest Markup Language",
+    options:[
+        "A. Highspeed text Mail Language",
+        "B. Hypertest Markup Language",
+        "C. Hyper Type Multi Language",
+        "D. Hyper Type Markup Language"
+    ]
+},
+{
+    num:2,
+    question:"What does CSS stand for?",
+    answer:"A. Cascading Style Sheet",
+    options:[
+        "A. Cascading Style Sheet",
+        "B. Casted Style Sheet",
+        "C. Casted Styling Sheet",
+        "D. Computer Style Sheet"
+    ]
+},
+{
+    num:3,
+    question:"HTML files are saved by default with the extension?",
+    answer:"C. .html",
+    options:[
+        "A. .ht",
+        "B. .h",
+        "C. .html",
+        "D. None of the above"
+    ]
+},
+{
+    num:4,
+    question:" What is the correct syntax to write an HTML comment?",
+    answer:"D. None of the above",
+    options:[
+        "A. /*Comment*/",
+        "B. // Comment",
+        "C. # Comment",
+        "D. None of the above"
+    ]
+},
+{
+    num:5,
+    question:"Which of the following is true about HTML tags?",
+    answer:"D. Not case sensitive",
+    options:[
+        "A. Are case sensitive",
+        "B. Are in upper case",
+        "C. Are in lower case",
+        "D. Not case sensitive"
+    ]
+},
+{
+    num:6,
+    question:"What is not a scripting language?",
+    answer:"A. Java",
+    options:[
+        "A. Java",
+        "B. Python",
+        "C. Node.js",
+        "D. PHP"
+    ]
+},
+{
+    num:7,
+    question:" What is the correct syntax for referring an external CSS?",
+    answer:"B. link href=(style.css) rel=(stylesheet)",
+    options:[
+        "A. style href=(style.css) rel=(stylesheet)",
+        "B. link href=(style.css) rel=(stylesheet)",
+        "C. Both A and B",
+        "D. Non of above"
+    ]
+},
+{
+    num:8,
+    question:"margin(25px 50px 75px 100px)",
+    answer:"C. Top,Right,Bottom,Left",
+    options:[
+        "A. Bottom,Left,Top,Right",
+        "B. Top,Left,Bottom,Right",
+        "C. Top,Right,Bottom,Left",
+        "D. Bottom,Right,Top,Left"
+    ]
+},{
+    num:9,
+    question:" Which attribute is used to provide a unique name to an HTML element?",
+    answer:"A. id",
+    options:[
+        "A. id",
+        "B. class",
+        "C. type",
+        "D. None of above"
+    ]
+},{
+    num:10,
+    question:"Which property is used to set colors in HTML?",
+    answer:"D. color",
+    options:[
+        "A. font-colour",
+        "B. font-color",
+        "C. colour",
+        "D. color"
+    ]
+}
+];
